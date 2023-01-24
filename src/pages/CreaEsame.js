6c59e7d255155c21ca386091627ae227c9438ae9
andres <ada.wong.gv@gmail.com>
2023-01-24 13:34:26 +0100

    removed unused files

--- src/pages/CreaEsame.js
+++ src/pages/CreaEsame.js
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 function CreaEsame(){
+
+    const location = useLocation()
+
+    useEffect(()=> {console.log("HO CAMBIATO PAGINA")},[location]);
+
     return(
         <div>
             PAGINA PER CREARE GLI ESAMI

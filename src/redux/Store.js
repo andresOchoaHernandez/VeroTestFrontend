a62d69b7b0d17dccdba4aa6332a93c9ff3d7ed5a
andres <ada.wong.gv@gmail.com>
2023-01-28 18:09:43 +0100

    setting up configurations to persist the redux state, in order to avoid having to login at every refresh

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,4 +1,4 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore} from "@reduxjs/toolkit";
 import { AuthenticationSlice } from "./AuthenticationSlice";
 import { CreateExamSlice } from "./CreateExamSlice";
 import { ExamCompletionSlice } from "./ExamCompletionSlice";
@@ -6,14 +6,16 @@ import { ExamExecutionSlice } from "./ExamExecutionSlice";
 import { ExamPresentationSlice } from "./ExamPresentationSlice";
 import { VeroTestApi } from "./VeroTestApi";
 
+const rootReducer = combineReducers({
+    [VeroTestApi.reducerPath] : VeroTestApi.reducer,
+    authentication : AuthenticationSlice.reducer,
+    createExam     : CreateExamSlice.reducer,
+    exampresentation: ExamPresentationSlice.reducer,
+    examexecution : ExamExecutionSlice.reducer,
+    examcompletion: ExamCompletionSlice.reducer
+});
+
 export const Store = configureStore({
-    reducer : {
-        [VeroTestApi.reducerPath] : VeroTestApi.reducer,
-        authentication : AuthenticationSlice.reducer,
-        createExam     : CreateExamSlice.reducer,
-        exampresentation: ExamPresentationSlice.reducer,
-        examexecution : ExamExecutionSlice.reducer,
-        examcompletion: ExamCompletionSlice.reducer
-    },
+    reducer : rootReducer,
     middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(VeroTestApi.middleware)
 });
